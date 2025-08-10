d99ca8e12152710a4d4c6e506a1ae3a18afcbdb2
Yu-Wei, Tseng <dadavidtseng@gmail.com>
2025-08-10 00:38:48 -0500

    feat: broadcasting event using `NoteGameInstanceSubsystem` and blueprint's event dispatcher (haven't decided which to use yet)

---
+++ Source/NapOfTheVR/NoteGameInstanceSubsystem.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NoteGameInstanceSubsystem.h"
+
+void UNoteGameInstanceSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+}
+
+void UNoteGameInstanceSubsystem::Deinitialize()
+{
+	OnSpawnModelButtonClicked.Clear();
+	Super::Deinitialize();
+}
+
+// UNoteGameInstanceSubsystem* UNoteGameInstanceSubsystem::Get(const UObject* WorldContext)
+// {
+// 	if (!WorldContext)
+// 	{
+// 		UE_LOG(LogTemp, Error, TEXT("WorldContext is null in UGlobalEventSubsystem::Get"));
+// 		return nullptr;
+// 	}
+//
+// 	const UWorld* World = GEngine->GetWorldFromContextObject(WorldContext, EGetWorldErrorMode::LogAndReturnNull);
+// 	if (!World)
+// 	{
+// 		UE_LOG(LogTemp, Error, TEXT("Failed to get World from WorldContext"));
+// 		return nullptr;
+// 	}
+//
+// 	UGameInstance* GameInstance = World->GetGameInstance();
+// 	if (!GameInstance)
+// 	{
+// 		UE_LOG(LogTemp, Error, TEXT("GameInstance is null"));
+// 		return nullptr;
+// 	}
+//
+// 	return GameInstance->GetSubsystem<UNoteGameInstanceSubsystem>();
+// }
+
+void UNoteGameInstanceSubsystem::TriggerSpawnModelButtonClicked()
+{
+	OnSpawnModelButtonClicked.Broadcast();
+}
---
+++ Source/NapOfTheVR/NoteGameInstanceSubsystem.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "NoteGameInstanceSubsystem.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnSpawnModelButtonClicked);
+
+/**
+ * 
+ */
+UCLASS()
+class NAPOFTHEVR_API UNoteGameInstanceSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	// USubsystem interface
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+
+	// // 取得子系統實例的靜態函數
+	// UFUNCTION(BlueprintCallable, Category = "Global Events")
+	// static UNoteGameInstanceSubsystem* Get(const UObject* WorldContext);
+
+	UPROPERTY(BlueprintAssignable, Category = "Global Events")
+	FOnSpawnModelButtonClicked OnSpawnModelButtonClicked;
+	UFUNCTION(BlueprintCallable, Category = "Global Events")
+	void TriggerSpawnModelButtonClicked();
+};
