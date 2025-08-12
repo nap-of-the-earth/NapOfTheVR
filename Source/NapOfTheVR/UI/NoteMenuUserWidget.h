08d255315a2e9976b274c7834f85fcb914c251a5
Yu-Wei, Tseng <dadavidtseng@gmail.com>
2025-08-11 22:25:06 -0500

    refactor: integrate `WBP_Menu` using `NoteMenuUserWidget` in c++

--- Source/NapOfTheVR/UI/NoteMenuUserWidget.h
+++ Source/NapOfTheVR/UI/NoteMenuUserWidget.h
@@ -9,9 +9,25 @@
 #include <Blueprint/UserWidget.h>
 #include <NoteMenuUserWidget.generated.h>
 
+//-Forward-Declaration--------------------------------------------------------------------------------
+class UButton;
+
 //----------------------------------------------------------------------------------------------------
 UCLASS()
 class NAPOFTHEVR_API UNoteMenuUserWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+protected:
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ExitButton;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* ResetOrientationButton;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* RestartButton;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	UButton* SpawnModelButton;
 };
